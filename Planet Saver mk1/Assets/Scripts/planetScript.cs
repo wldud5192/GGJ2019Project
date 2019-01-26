d8b02e7109332936011d73afafada4b798d43dab
Cameron <cameron.dodds282@gmail.com>
2019-01-26 12:23:12 +1000

    fixed the merging into a simgle scene

---
+++ Planet Saver mk1/Assets/Scripts/planetScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetScript : MonoBehaviour
+{
+    //basic health stats
+    public int maxHealth = 5; // health will go from 5 down to 0
+    public int minHealth = 0; 
+    public int minBuildingLevel = 0; // will go up to 5
+    public int maxBuildingLevel = 5;
+
+    //current stats
+    public int currentHealth;
+    public int currentLevel;
+
+    public Collider2D colliderA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        currentHealth = maxHealth;
+        currentLevel = minBuildingLevel;
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //this code will make sure that the death event happens if the health is 0
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+
+    }
+    //the method to take damage
+    public void TakeDamage()
+    {
+        // if there are buildings they will be taken down a level
+        if (currentLevel > minBuildingLevel)
+        {
+            currentLevel = currentLevel - 1;
+        }
+        //if not then the planet will take damage
+        if (currentHealth > minHealth)
+        {
+            currentHealth = currentHealth-1;
+
+            Debug.Log("planet hurt");
+        }
+        //otherwise there must be something wrong
+        else
+        {
+            currentHealth = minHealth;
+            Debug.Log("why is the planet not dead");
+        }
+    }
+
+    public void HealPlanet()
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth = currentHealth + 1;
+            Debug.Log("planet healed");
+        }
+        else
+        {
+            currentHealth = maxHealth;
+            Debug.Log("the planet is at full health");
+        }
+
+    }
+
+    public void BuildUp()
+    {
+        if (currentLevel < maxBuildingLevel)
+        {
+            currentLevel = currentLevel +1;
+            Debug.Log("built up!");
+        }
+        else
+        {
+            currentLevel = minBuildingLevel;
+            Debug.Log("the planet is at full health");
+        }
+    }
+
+    public void Death(){
+        Debug.Log("You Dead!");
+    }
+}
