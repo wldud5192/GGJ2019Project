36cb4eea3b6adf21f9a24d035b9847190f59d3c6
David Tran <davidhoangt@gmail.com>
2019-01-26 11:58:58 +1000

    DUDE1

---
+++ Planet Saver mk1/Assets/Scripts/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+	public float fireRate = 0;
+	public float Damage = 10;
+	public LayerMask WhatToHit;
+
+	float timeToFire = 0;
+	Transform firePoint;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+    	firePoint = transform.Find("FirePoint");
+    	if (firePoint == null) {
+    		Debug.LogError("No FirePoint");
+    	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Shoot();
+    	if (fireRate == 0){
+    		if (Input.GetKeyDown(KeyCode.S))	{
+    			Shoot();
+    		}
+    	}    
+    	else {
+    		if (Input.GetKeyDown(KeyCode.S) && Time.time > timeToFire)	{
+    			timeToFire = Time.time + 1/fireRate;
+    			Shoot();
+			}
+    	}
+    }
+
+    void Shoot()	{
+   		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+    	Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
+    	RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100, WhatToHit);
+    	// distance adjusting of ray here
+    	Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition)*100, Color.red);
+    	if (hit.collider != null)	{
+    		Debug.DrawLine(firePointPosition, hit.point, Color.blue);
+    	}
+    }
+}
---
+++ Planet Saver mk1/Assets/Scripts/dudeMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class dudeMove : MonoBehaviour {
+
+	public GameObject Planet;
+	public float speed;
+	// private Vector3 endPosition = new Vector3(-6, -4, 0);
+
+    void Start(){
+    }
+
+    void Update() 
+    {
+    	MoveAround();
+
+  		// if(transform.position != endPosition) {
+    //     	transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+    //     }
+    }
+
+    void MoveAround()
+    {
+
+	if (Input.GetKey (KeyCode.A)) {
+	 	transform.RotateAround(Planet.transform.position, Vector3.forward, speed * Time.deltaTime); //LEFT
+		}
+	if (Input.GetKey (KeyCode.D)) {
+	 	transform.RotateAround(Planet.transform.position, Vector3.back, speed * Time.deltaTime); //RIGHT
+		}
+
+	}
+
+    // rotationZ = Mathf.Clamp (rotationZ, -90, 90);
+    // transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, -rotationZ);
+
+
+	// float maxX = 4;
+	// float maxY = 4;
+
+
+	// Vector3 pos = transform.position;
+	// pos.x = Mathf.Clamp (pos.x, -maxX, maxX);
+	// pos.y = Mathf.Clamp (pos.y, -maxY, maxY);
+	// transform.position = pos;
+
+}
